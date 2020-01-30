63fcc8f980c3be5eed256098a63bcd59956f3c2a
Finn <finnterdal@gmail.com>
2020-01-30 15:55:42 -0600

    clean up

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
 const express = require("express")
 const morgan = require("morgan")
-// const { Dog } = require("./database")
-const dogRoutes = require("./api/dogs")
 
 const app = express()
 app.use(morgan("dev"))
-
-app.get("/", async (req, res, next) => {
-  res.send(`
-    <a href="/api/dogs">Go to dogs</a>
-    <a href="/api/dogs/1">Go to dog 1</a>
-  `)
-})
-
-app.use("/api/dogs", dogRoutes)
 
 const PORT = 3030
 app.listen(PORT, () => {
